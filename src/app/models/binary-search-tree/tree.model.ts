453cc1cf323ad3091bc1aca14841a3ad54b2291e
petrolingus <petrolingus@gmail.com>
2021-04-18 04:57:08 +0300

    added insert and insertionRecursive methods to BinarySearchTree class

--- src/app/models/binary-search-tree/tree.model.ts
+++ src/app/models/binary-search-tree/tree.model.ts
@@ -2,4 +2,22 @@ import {TreeNode} from './node.model';
 
 export class BinarySearchTree<K, V> {
     tree: TreeNode<K, V>;
+
+    public insert(key: K, value: V): void {
+        this.tree = this.insertionRecursive(this.tree, key, value);
+    }
+
+    private insertionRecursive(root: TreeNode<K, V>, key: K, value: V): TreeNode<K, V> {
+        if (root == null) {
+            return new TreeNode<K, V>(key, value);
+        }
+
+        if (key < root.key) {
+            root.left = this.insertionRecursive(root.left, key, value);
+        } else if (key > root.key) {
+            root.right = this.insertionRecursive(root.right, key, value);
+        }
+
+        return root;
+    }
 }
